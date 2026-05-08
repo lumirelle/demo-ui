6873b27c92e320739c41e36862269a9ec1e362a1
lumirelle <shabbyacc@outlook.com>
2026-05-08 10:10:58 +0800

    feat: init project

---
+++ knip.config.ts
@@ -0,0 +1,6 @@
+import type { KnipConfig } from 'knip'
+
+export default {
+  /// keep-sorted
+  ignoreDependencies: ['@arethetypeswrong/cli', '@lumirelle/oxlint-config', 'publint'],
+} satisfies KnipConfig
